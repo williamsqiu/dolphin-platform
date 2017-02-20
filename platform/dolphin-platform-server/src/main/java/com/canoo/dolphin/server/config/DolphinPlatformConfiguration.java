f93cbd16bbd8d93a7e6a5c357200206e5b16f659
kunal singh <kunal.singh@canoo.com>
2017-02-20 12:51:07 +0530

    Fix compilation error

--- platform/dolphin-platform-server/src/main/java/com/canoo/dolphin/server/config/DolphinPlatformConfiguration.java
+++ platform/dolphin-platform-server/src/main/java/com/canoo/dolphin/server/config/DolphinPlatformConfiguration.java
@@ -20,10 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.logging.Level;
 
 /**
