d7e350e12e7fc188c2344ef776b0ac652619c7b7
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-04-04 11:36:19 +0200

    version 0.11.0

--- integration-tests/src/test/java/com/canoo/dolphin/integration/AbstractIntegrationTest.java
+++ integration-tests/src/test/java/com/canoo/dolphin/integration/AbstractIntegrationTest.java
@@ -64,7 +64,7 @@ public class AbstractIntegrationTest {
 
     protected <T> ControllerProxy<T> createController(ClientContext clientContext, String controllerName) {
         try {
-            return (ControllerProxy<T>) clientContext.createController(controllerName).get(2, TimeUnit.MINUTES);
+            return (ControllerProxy<T>) clientContext.createController(controllerName).get(4, TimeUnit.MINUTES);
         } catch (Exception e) {
             throw new DolphinRemotingException("Can not create controller " + controllerName, e);
         }
