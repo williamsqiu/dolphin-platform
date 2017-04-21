61f9bbecc0a48527b6f43fd7b50ec1b9cc59c7c1
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-04-21 13:05:50 +0200

    Additional logging

--- platform-extras/dolphin-platform-distributed-eventbus/src/main/java/com/canoo/dolphin/server/event/DistributedEventBusProvider.java
+++ platform-extras/dolphin-platform-distributed-eventbus/src/main/java/com/canoo/dolphin/server/event/DistributedEventBusProvider.java
@@ -4,11 +4,15 @@ import com.canoo.dolphin.server.bootstrap.DolphinPlatformBootstrap;
 import com.canoo.dolphin.server.config.DolphinPlatformConfiguration;
 import com.canoo.dolphin.server.event.impl.EventBusProvider;
 import com.canoo.dolphin.util.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
 public class DistributedEventBusProvider implements EventBusProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DistributedEventBusProvider.class);
 
     public static final String DISTRIBUTED_EVENTBUS_NAME = "distributed";
 
@@ -18,6 +22,8 @@ public class DistributedEventBusProvider implements EventBusProvider {
     }
 
     public DolphinEventBus create(final DolphinPlatformConfiguration configuration) {
+        LOG.debug("creating distributed event bus");
+
         HazelcastProvider hazelcastProvider = null;
         Iterator<HazelcastProvider> iterator = ServiceLoader.load(HazelcastProvider.class).iterator();
 
@@ -25,7 +31,12 @@ public class DistributedEventBusProvider implements EventBusProvider {
         if(iterator.hasNext()) {
             hazelcastProvider = iterator.next();
         }
+        if(hazelcastProvider == null) {
+            LOG.error("No hazelcast provider found!");
+        }
         Assert.requireNonNull(hazelcastProvider, "hazelcastProvider");
+
+        LOG.error("Using Hazelcast provider {}", hazelcastProvider.getClass());
 
         return new DistributedEventBus(hazelcastProvider.getHazelcastInstance(configuration), DolphinPlatformBootstrap.getSessionProvider(), DolphinPlatformBootstrap.getSessionLifecycleHandler());
     }
