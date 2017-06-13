f21d5b310259c99413d33d132bd494adec559e41
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-06-13 09:11:35 +0200

    client private & public

--- platform/dolphin-platform-core/src/main/java/com/canoo/impl/platform/core/ReflectionHelper.java
+++ platform/dolphin-platform-core/src/main/java/com/canoo/impl/platform/core/ReflectionHelper.java
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.canoo.dolphin.impl;
+package com.canoo.impl.platform.core;
 
-import com.canoo.dolphin.util.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
