abb4653649b6eda2e9ee6364bd03101b3e156bc9
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-08-01 15:27:18 +0200

    More lazy

--- platform/dolphin-platform-remoting-server/src/main/java/com/canoo/dp/impl/server/event/LazyEventBusInvocationHandler.java
+++ platform/dolphin-platform-remoting-server/src/main/java/com/canoo/dp/impl/server/event/LazyEventBusInvocationHandler.java
@@ -2,6 +2,7 @@ package com.canoo.dp.impl.server.event;
 
 import com.canoo.dp.impl.server.bootstrap.PlatformBootstrap;
 import com.canoo.platform.remoting.server.event.DolphinEventBus;
+import com.canoo.platform.server.spi.ServerCoreComponents;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -12,9 +13,12 @@ public class LazyEventBusInvocationHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        final DolphinEventBus instance = PlatformBootstrap.getServerCoreComponents().getInstance(DolphinEventBus.class);
-        if (instance != null) {
-            return method.invoke(instance, args);
+        final ServerCoreComponents serverCoreComponents = PlatformBootstrap.getServerCoreComponents();
+        if(serverCoreComponents != null) {
+            final DolphinEventBus instance = serverCoreComponents.getInstance(DolphinEventBus.class);
+            if (instance != null) {
+                return method.invoke(instance, args);
+            }
         }
         if(method.getDeclaringClass().equals(Object.class)) {
             return method.invoke(DUMMY_OBJECT, args);
