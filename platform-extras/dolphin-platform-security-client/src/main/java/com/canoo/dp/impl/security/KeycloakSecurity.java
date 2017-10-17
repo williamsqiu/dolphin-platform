634e71ac49f4ab733cf2382f313f6eac9f32b6c3
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-10-17 15:35:56 +0200

    notes based on discussion with sebastian blanc

--- platform-extras/dolphin-platform-security-client/src/main/java/com/canoo/dp/impl/security/KeycloakSecurity.java
+++ platform-extras/dolphin-platform-security-client/src/main/java/com/canoo/dp/impl/security/KeycloakSecurity.java
@@ -29,6 +29,19 @@ public class KeycloakSecurity implements Security {
     @Override
     public Future<Void> login(final String user, final String password) {
         return backgroundExecutor.submit(() -> {
+            //This is the ADMIN login
+            //It would be better to just do a REST request.
+
+            //Sample: https://github.com/aerogear/aerogear-unifiedpush-server/blob/master/node.js/directgranttest.js
+
+            //Result contains refresh token and Access token.
+            //Access Token contains exprire date
+            //if this reached do call with refresh token first.
+
+            //https://github.com/keycloak/keycloak/tree/master/adapters/oidc/js/src/main/resources
+
+            //Setup for the realm / client -> "Direct Grand" must be enabled in keycloak admin
+
             final Keycloak keycloak = Keycloak.getInstance(
                     authEndpoint,
                     realmName,
@@ -43,11 +56,19 @@ public class KeycloakSecurity implements Security {
     @Override
     public Future<Void> logout() {
         return backgroundExecutor.submit(() -> {
+            //REST Endpoint at Keycloak site -> Call must be done manually
+            //http://www.keycloak.org/docs-api/3.3/rest-api/index.html
+            //POST /admin/realms/{realm}/users/{id}/logout
+
             throw new RuntimeException("Not yet implemented");
         });
     }
 
     public boolean isAuthorized() {
+        //REST Endpoint at Keycloak site -> Call must be done manually
+        //http://www.keycloak.org/docs-api/3.3/rest-api/index.html
+        //See /admin/realms/{realm}/users/{id}/sessions
+
         return accessToken != null;
     }
 
