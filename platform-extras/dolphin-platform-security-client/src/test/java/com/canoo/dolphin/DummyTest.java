2acd0b61ac36bfc97bc76a41f82909531b30cd8b
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-10-27 17:53:07 +0200

    dummy tests

---
+++ platform-extras/dolphin-platform-security-client/src/test/java/com/canoo/dolphin/DummyTest.java
@@ -0,0 +1,15 @@
+package com.canoo.dolphin;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by hendrikebbers on 27.10.17.
+ */
+public class DummyTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
