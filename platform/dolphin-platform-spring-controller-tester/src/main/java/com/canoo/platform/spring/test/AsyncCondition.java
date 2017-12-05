a397651f351b3268cb63e71260663a17facd3faf
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-12-05 16:32:21 +0100

    meta info

--- platform/dolphin-platform-spring-controller-tester/src/main/java/com/canoo/platform/spring/test/AsyncCondition.java
+++ platform/dolphin-platform-spring-controller-tester/src/main/java/com/canoo/platform/spring/test/AsyncCondition.java
@@ -1,10 +1,16 @@
 package com.canoo.platform.spring.test;
 
+import org.apiguardian.api.API;
+
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static org.apiguardian.api.API.Status.EXPERIMENTAL;
+
+@API(since = "1.0.0-RC2", status = EXPERIMENTAL)
 public interface AsyncCondition {
 
+    @Deprecated
     void await() throws InterruptedException;
 
     void await(long time, TimeUnit unit) throws InterruptedException, TimeoutException;
