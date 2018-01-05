0442b379f5075c7dd1d06ddb3f7a29513768ade8
Hendrik Ebbers <hendrik.ebbers@web.de>
2018-01-05 13:59:46 +0100

    removed logging usage from todo sample

--- platform-examples/todo-example/todo-client/src/main/java/com/canoo/dolphin/todo/client/ToDoClient.java
+++ platform-examples/todo-example/todo-client/src/main/java/com/canoo/dolphin/todo/client/ToDoClient.java
@@ -15,23 +15,12 @@
  */
 package com.canoo.dolphin.todo.client;
 
-import com.canoo.platform.logging.DolphinLoggerConfiguration;
-import com.canoo.impl.dp.logging.DolphinLoggerFactory;
-import com.canoo.platform.logging.spi.LogMessage;
-import com.canoo.platform.logger.client.util.LogClientUtil;
-import com.canoo.platform.logger.client.view.LogListCell;
 import com.canoo.platform.remoting.client.ClientContext;
 import com.canoo.platform.remoting.client.javafx.DolphinPlatformApplication;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.slf4j.event.Level;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -50,30 +39,10 @@ public class ToDoClient extends DolphinPlatformApplication {
         scene.getStylesheets().add(ToDoClient.class.getResource("style.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setOnCloseRequest(e -> System.exit(0));
-
-        Stage logStage = new Stage();
-        ListView<LogMessage> listView = new ListView<>();
-        listView.setCellFactory(v -> new LogListCell());
-        listView.setItems(LogClientUtil.createObservableListFromLocalCache());
-        Button clearCacheButton = new Button("clear cache");
-        clearCacheButton.setOnAction(e -> DolphinLoggerFactory.clearCache());
-        VBox logPane = new VBox(listView, clearCacheButton);
-        logPane.setFillWidth(true);
-        logPane.setPadding(new Insets(6));
-        logPane.setSpacing(6);
-        VBox.setVgrow(listView, Priority.ALWAYS);
-        VBox.setVgrow(clearCacheButton, Priority.NEVER);
-        logStage.setScene(new Scene(logPane));
-        logStage.show();
-
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        DolphinLoggerConfiguration configuration = new DolphinLoggerConfiguration();
-        configuration.setGlobalLevel(Level.TRACE);
-        DolphinLoggerFactory.applyConfiguration(configuration);
-
         Platform.setImplicitExit(false);
         Application.launch(args);
     }
