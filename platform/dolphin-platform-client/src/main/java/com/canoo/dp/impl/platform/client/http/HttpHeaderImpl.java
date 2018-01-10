ee60f98be90cde39f7ec0ac9939292354b0c31db
giftkugel <github@giftkugel.de>
2018-01-10 11:46:18 +0100

    Typo

--- platform/dolphin-platform-client/src/main/java/com/canoo/dp/impl/platform/client/http/HttpHeaderImpl.java
+++ platform/dolphin-platform-client/src/main/java/com/canoo/dp/impl/platform/client/http/HttpHeaderImpl.java
@@ -9,7 +9,7 @@ public class HttpHeaderImpl implements HttpHeader {
     private final String content;
 
     public HttpHeaderImpl(final String name, final String content) {
-        //No null checks since the HTPP version is defined as header with no / null name
+        //No null checks since the HTTP version is defined as header with no / null name
         this.name = name;
         this.content = content;
     }
