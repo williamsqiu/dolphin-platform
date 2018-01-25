6a15b1df450485f3394b030327e9c5b57adb5f6e
Hendrik Ebbers <hendrik.ebbers@web.de>
2018-01-25 08:14:05 +0100

    keycloak download

--- services/downloadArtifacts.sh
+++ services/downloadArtifacts.sh
@@ -1,7 +1,4 @@
 g#!/bin/bash
-cd graylog
-wget https://www.dropbox.com/s/lwreupx6ivcplzt/graylog-2.3.2.tgz
-cd $OLDPWD
 cd keycloak/ansible
 wget https://www.dropbox.com/s/qtp7b0622j6a5i1/keycloak-3.4.0.Final.tar
 cd $OLDPWD
