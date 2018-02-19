27b2fcc989077956b8d1a3bd8121570c8e53c16a
Hendrik Ebbers <hendrik.ebbers@web.de>
2018-02-19 07:49:50 +0100

    New response wrapper is working in Spring & JavaEE

--- platform/dolphin-platform-server/src/main/java/com/canoo/dp/impl/server/servlet/ServerTimingFilter.java
+++ platform/dolphin-platform-server/src/main/java/com/canoo/dp/impl/server/servlet/ServerTimingFilter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015-2018 Canoo Engineering AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.canoo.dp.impl.server.servlet;
 
 import com.canoo.dp.impl.server.timing.ServerTimingImpl;
@@ -35,7 +50,7 @@ public class ServerTimingFilter implements Filter {
 
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
-        final GenericResponseWrapper responseWrapper = new GenericResponseWrapper((HttpServletResponse) response);
+        final HttpServletResponseCopier responseWrapper = new HttpServletResponseCopier((HttpServletResponse) response);
 
         final ServerTimingImpl timing = new ServerTimingImpl();
         timingLocal.set(timing);
@@ -50,7 +65,7 @@ public class ServerTimingFilter implements Filter {
                 timing.clear();
             }
             timingLocal.set(null);
-            responseWrapper.updateWrappedResponse();
+            responseWrapper.flushBuffer();
         }
     }
 
