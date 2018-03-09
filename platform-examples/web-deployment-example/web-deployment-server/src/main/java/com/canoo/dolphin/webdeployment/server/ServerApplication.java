97dbb002a69e45deaa5557ad8537d0e23a90f28d
kunal singh <kunal.singh@canoo.com>
2018-03-09 17:28:47 +0530

    extends web-deployment-server ServerApplication from SpringBootServletInitializer

--- platform-examples/web-deployment-example/web-deployment-server/src/main/java/com/canoo/dolphin/webdeployment/server/ServerApplication.java
+++ platform-examples/web-deployment-example/web-deployment-server/src/main/java/com/canoo/dolphin/webdeployment/server/ServerApplication.java
@@ -17,9 +17,10 @@ package com.canoo.dolphin.webdeployment.server;
 
 import com.canoo.platform.remoting.server.spring.DolphinPlatformRemotingApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @DolphinPlatformRemotingApplication
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(new Class[]{ServerApplication.class}, args);
