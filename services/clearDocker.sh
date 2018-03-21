fe0fc9833d7c922b63127f2e06309c1132902468
giftkugel <github@giftkugel.de>
2018-03-21 12:18:41 +0100

    Cleanup script will now remove the keycloak containers and images

--- services/clearDocker.sh
+++ services/clearDocker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-docker rm -f $(docker ps -a -q)
-docker rmi -f $(docker images -q)
+docker rm -f $(docker ps -a -q -f "name=services_keycloak-server")
+docker rmi -f $(docker images -q -f "reference=services_keycloak-server" )
+docker rmi -f $(docker images -q -f "reference=dolphin-services/base")
 
 echo "Images"
 docker images
