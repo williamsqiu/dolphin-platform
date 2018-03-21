e16f3ada99360ad2b505527d6ae310aeb2cb209f
giftkugel <github@giftkugel.de>
2018-03-21 12:31:10 +0100

    Scripts updated

--- services/buildBase.sh
+++ services/buildBase.sh
@@ -1,1 +1,8 @@
-docker build -t dolphin-services/base base/
+#!/bin/bash
+if [ -f "keycloak/ansible/keycloak-3.4.0.Final.tar" ]
+then
+	docker build -t dolphin-services/base base/
+else
+	echo "Please download artifacts first"
+fi
+
